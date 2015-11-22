4708f7b9a2b21e77613b49e66ea5fbd5e5ac5fe1
Michael Menz <michael.menz.developer@gmail.com>
2015-11-22 18:55:05 -0500

    Working on bug fix for dealing with possession where a charge occurs

--- Basketball/PostChargeTracker.py
+++ Basketball/PostChargeTracker.py
@@ -1,7 +1,8 @@
 # PostChargeTracker.py
 
 import PostEventTracker as PET
-from utilities import empty
+from utilities.utilities import empty
+from scipy.stats import ttest_ind
 
 csvfile = "[10-20-2013]-[06-30-2014]-combined-stats-with-possessions.csv"
 
@@ -15,5 +16,22 @@ def points_evaluator(rows):
 			total_points += int(row['points'])
 	return total_points
 
-PCT = PET.PostEventTracker(csvfile, charge_function, points_evaluator, 6)
-PCT.plotDifferences()
+PCT = PET.PostEventTracker(csvfile, charge_function, points_evaluator, 2)
+aftercharge = PCT.scorePerPossessionAfterEvent()
+baseline = PCT.scorePerPossessionFullGame()
+
+away_after_charge_spps = []
+away_spps = []
+for game in aftercharge:
+	if aftercharge[game]['away']['spp']['own'] != None:
+		for i in range(aftercharge[game]['away']['spp']['own'][1]):
+			print aftercharge[game]['away']['spp']['own'][0]
+			away_after_charge_spps.append( aftercharge[game]['away']['spp']['own'][0] / float(aftercharge[game]['away']['spp']['own'][1]))
+	away_spps.append( baseline[game]['away']['spp'][0] / float(baseline[game]['away']['spp'][1]))
+
+print len(away_after_charge_spps)
+print sum(away_after_charge_spps) / len(away_after_charge_spps)
+print len(away_spps)
+print sum(away_spps) / len(away_spps)
+
+print ttest_ind(away_spps, away_after_charge_spps, equal_var=False)
--- Basketball/PostEventTracker.py
+++ Basketball/PostEventTracker.py
@@ -1,19 +1,11 @@
 # PostEventTracker.py
 
 import csv
-import homeAwayLookup as HAL
-from utilities import empty
-
-CAN_PLOT = True
-try:
-	import matplotlib.pyplot as plt
-except:
-	CAN_PLOT = False
-
+from utilities.utilities import empty, homeAwayLookup
 
 class PostEventTracker:
 
-	def __init__(self, csvfile, event_function, possession_evaluator, possessions_to_track):
+	def __init__(self, csvfile, event_function, possession_evaluator, possessions_to_track, include_event=False):
 		'''
 		PostEventTracker class
 			Given a csvfile with events, an event_function to evaluate whether a row contains a specific event, 
@@ -25,6 +17,7 @@ class PostEventTracker:
 		self.ef = event_function
 		self.pe = possession_evaluator
 		self.ptt = possessions_to_track
+		self.include_event = include_event
 
 	def scorePerPossessionFullGame(self):
 		'''
@@ -63,8 +56,8 @@ class PostEventTracker:
 						avg_scores = {'home':{}, 'away':{}}
 						avg_scores['home']['team'] = total_scores['home']['team']
 						avg_scores['away']['team'] = total_scores['away']['team']
-						avg_scores['home']['spp'] = total_scores['home']['total'] / float(total_scores['home']['possessions'])
-						avg_scores['away']['spp'] = total_scores['away']['total'] / float(total_scores['away']['possessions'])
+						avg_scores['home']['spp'] = (total_scores['home']['total'] , total_scores['home']['possessions'])
+						avg_scores['away']['spp'] = (total_scores['away']['total'] , total_scores['away']['possessions'])
 
 						returnDict[current_game] = avg_scores
 
@@ -75,7 +68,7 @@ class PostEventTracker:
 							'home': {'team': None, 'total': 0, 'possessions': 0},
 							'away': {'team': None, 'total': 0, 'possessions': 0}
 						}
-					teams = HAL.lookup(current_game)
+					teams = homeAwayLookup(current_game)
 					total_scores['home']['team'] = teams['home']
 					total_scores['away']['team'] = teams['away']
 					current_possession_number = -1
@@ -109,8 +102,8 @@ class PostEventTracker:
 		'''
 		Returns a dictionary where keys are game_ids and values are dictionaries of the form:
 			{
-				'home':{'team': TEAM_NAME, 'spp': SCORE_PER_POSSESSION},
-				'away':{'team': TEAM_NAME, 'spp': SCORE_PER_POSSESSION}
+				'home':{'team': TEAM_NAME, 'spp': {'own': SCORE_PER_POSSESSION, 'opp': SCORE_PER_POSSESSION}},
+				'away':{'team': TEAM_NAME, 'spp': {'own': SCORE_PER_POSSESSION, 'opp': SCORE_PER_POSSESSION}}
 			}
 		'''
 		returnDict = {}
@@ -125,6 +118,8 @@ class PostEventTracker:
 		totaler = None
 		current_possession = []
 		event_counter = 0
+		whose_event = None
+		change_event_counter = False
 
 		with open(self.csvfile) as csvfile:
 			reader = csv.DictReader(csvfile)
@@ -135,23 +130,38 @@ class PostEventTracker:
 
 					# evaluate the last possession from the previous game
 					if current_possession_number != -1 and event_counter > 0:
-						totaler['total'] += self.pe(current_possession)
-						totaler['possessions'] += 1
+						if whose_event == totaler['team']:
+							totaler['total']['own'] += self.pe(current_possession)
+							totaler['possessions']['own'] += 1
+						else:
+							totaler['total']['opp'] += self.pe(current_possession)
+							totaler['possessions']['opp'] += 1
 						event_counter -= 1
 
 					# add the game to the return dictionary
 					if current_game != None:
-						avg_scores = {'home':{}, 'away':{}}
+						avg_scores = {'home':{'spp':{}}, 'away':{'spp':{}}}
 						avg_scores['home']['team'] = total_scores['home']['team']
 						avg_scores['away']['team'] = total_scores['away']['team']
-						if total_scores['home']['possessions'] != 0:
-							avg_scores['home']['spp'] = total_scores['home']['total'] / float(total_scores['home']['possessions'])
+						if total_scores['home']['possessions']['own'] != 0:
+							avg_scores['home']['spp']['own'] = (total_scores['home']['total']['own'] , total_scores['home']['possessions']['own'])
 						else:
-							avg_scores['home']['spp'] = None
-						if total_scores['away']['possessions'] != 0:
-							avg_scores['away']['spp'] = total_scores['away']['total'] / float(total_scores['away']['possessions'])
+							avg_scores['home']['spp']['own'] = None
+
+						if total_scores['home']['possessions']['opp'] != 0:
+							avg_scores['home']['spp']['opp'] = (total_scores['home']['total']['opp'] , total_scores['home']['possessions']['opp'])
 						else:
-							avg_scores['away']['spp'] = None
+							avg_scores['home']['spp']['opp'] = None
+
+						if total_scores['away']['possessions']['own'] != 0:
+							avg_scores['away']['spp']['own'] = (total_scores['away']['total']['own'] , total_scores['away']['possessions']['own'])
+						else:
+							avg_scores['away']['spp']['own'] = None
+
+						if total_scores['away']['possessions']['opp'] != 0:
+							avg_scores['away']['spp']['opp'] = (total_scores['away']['total']['opp'] , total_scores['away']['possessions']['opp'])
+						else:
+							avg_scores['away']['spp']['opp'] = None
 
 						returnDict[current_game] = avg_scores
 
@@ -159,20 +169,17 @@ class PostEventTracker:
 					current_game = row['game_id']
 					game_counter += 1
 					total_scores = {
-							'home': {'team': None, 'total': 0, 'possessions': 0},
-							'away': {'team': None, 'total': 0, 'possessions': 0}
+							'home': {'team': None, 'total': {'opp':0, 'own':0}, 'possessions': {'opp':0, 'own':0}},
+							'away': {'team': None, 'total': {'opp':0, 'own':0}, 'possessions': {'opp':0, 'own':0}}
 						}
-					teams = HAL.lookup(current_game)
+					teams = homeAwayLookup(current_game)
 					total_scores['home']['team'] = teams['home']
 					total_scores['away']['team'] = teams['away']
 					current_possession_number = -1
 					totaler = None
 					event_counter = 0
 					current_possession = []
-
-				# check if we have an event:
-				if self.ef(row):
-					event_counter = self.ptt
+					whose_event = None
 
 				# ignore lines without a possession
 				if empty(row['possession_number']):
@@ -181,9 +188,13 @@ class PostEventTracker:
 				# check if we have a new possession
 				if row['possession_number'] != current_possession_number:
 					# add score for the possession
-					if current_possession_number != -1 and event_counter > 0:
-						totaler['total'] += self.pe(current_possession)
-						totaler['possessions'] += 1
+					if current_possession_number != -1 and event_counter > 0 and current_possession != []:
+						if whose_event == totaler['team']:
+							totaler['total']['own'] += self.pe(current_possession)
+							totaler['possessions']['own'] += 1
+						else:
+							totaler['total']['opp'] += self.pe(current_possession)
+							totaler['possessions']['opp'] += 1
 						event_counter -= 1
 
 					# reset possession variables
@@ -194,42 +205,52 @@ class PostEventTracker:
 					current_possession_number = row['possession_number']
 					current_possession = []
 
+				if change_event_counter:
+					event_counter = change_event_counter[0]
+					whose_event = change_event_counter[1]
+					change_event_counter = False
+
+				# check if we have an event:
+				if self.ef(row):
+					change_event_counter = [self.ptt, row['possession_team']]
+					
 				current_possession.append(row)
+
 
 		return returnDict
 
-	def plotDifferences(self):
-		'''
-		Show a scatter plot of games colored by team of the spp after events
-			and in general
-		'''
-		if not CAN_PLOT:
-			raise ImportError("Could not import matplotlib for plotting")
-		fullgame = self.scorePerPossessionFullGame()
-		afterevent = self.scorePerPossessionAfterEvent()
-		xs = []
-		ys = []
-		teams = []
-		for key in fullgame:
-			game_spp = fullgame[key]
-			after_spp = afterevent[key]
-			if after_spp['home']['spp'] != None:
-				xs.append( after_spp['home']['spp'] )
-				ys.append( game_spp['home']['spp'] )
-				teams.append( game_spp['home']['team'] )
+	# def plotDifferences(self):
+	# 	'''
+	# 	Show a scatter plot of games colored by team of the spp after events
+	# 		and in general
+	# 	'''
+	# 	if not CAN_PLOT:
+	# 		raise ImportError("Could not import matplotlib for plotting")
+	# 	fullgame = self.scorePerPossessionFullGame()
+	# 	afterevent = self.scorePerPossessionAfterEvent()
+	# 	xs = []
+	# 	ys = []
+	# 	teams = []
+	# 	for key in fullgame:
+	# 		game_spp = fullgame[key]
+	# 		after_spp = afterevent[key]
+	# 		if after_spp['home']['spp'] != None:
+	# 			xs.append( after_spp['home']['spp'] )
+	# 			ys.append( game_spp['home']['spp'] )
+	# 			teams.append( game_spp['home']['team'] )
 
-		# make a colormap
-		color_map = {}
-		counter = 0
-		colors = []
-		for team in teams:
-			if not team in color_map:
-				color_map[team] = counter / 30.0
-				counter += 1
-			colors.append(color_map[team])
+	# 	# make a colormap
+	# 	color_map = {}
+	# 	counter = 0
+	# 	colors = []
+	# 	for team in teams:
+	# 		if not team in color_map:
+	# 			color_map[team] = counter / 30.0
+	# 			counter += 1
+	# 		colors.append(color_map[team])
 
-		plt.scatter(xs, ys, c=colors)
-		plt.show()
+	# 	plt.scatter(xs, ys, c=colors)
+	# 	plt.show()
 
 
 
