0b607c44589b2749f91dae1337812a6ee6b93afb
Michael Menz <michael.menz.developer@gmail.com>
2016-10-12 23:34:51 -0400

    Switched from rpy2 to process call

--- Basketball/LiveGameModel/web_api/.profile.d/set_opt.sh
+++
@@ -1,2 +0,0 @@
-export PATH=/app/vendor/R/bin:/app/vendor/gcc-4.3/bin:/app/.heroku/python/bin:/usr/local/bin:/usr/bin:/bin
-export LD_LIBRARY_PATH=/usr/lib:/usr/local/lib:/app/vendor/R/lib64/R/modules:/app/vendor/R/lib64/R/lib:/app/vendor/gcc-4.3/lib64
--- Basketball/LiveGameModel/web_api/main.py
+++ Basketball/LiveGameModel/web_api/main.py
@@ -1,44 +1,20 @@
 from flask import Flask
-import rpy2.robjects as robjects
+# import rpy2.robjects as robjects
 from sklearn import neural_network
 import pickle
+import subprocess
+import os
 
 
 app = Flask(__name__)
 
 with open('no_possession_model.mdl') as serialized:
     model = pickle.loads(serialized.read())
-
-RCode = """
-# Parse for live scores: http://www.espn.com/nba/bottomline/scores
-
-y <- readLines('http://www.espn.com/nba/bottomline/scores')
-y <- strsplit(y,"&nba_s_left")
-y <- y[[1]]
-y <- gsub("%20", " ", y)
-y <- y[grep("^[0-9]", y)]
-
-
-gameid <- as.numeric(gsub(".*=([0-9][0-9]+).*", "\\\\1", y))
-team1 <- gsub(".*=([A-z ]+[A-z]) [0-9a].*", "\\\\1", y)
-team2 <- gsub("^[0-9]+=[A-z ]+ (at|[0-9]+) ([ ]*)([A-z ]+) [(0-9].*",
-              "\\\\3", y)
-score1 <- as.numeric(gsub(".*=[A-z ]+ ([0-9]+).*", "\\\\1", y))
-score2 <- as.numeric(gsub(".* ([0-9]+) [(].*", "\\\\1", y))
-minrem <- as.numeric(gsub(".*[(]([0-9]+):[0-9]+ I.*", "\\\\1", y))
-secrem <- as.numeric(gsub(".*[(][0-9]+:([0-9]+) I.*", "\\\\1", y))
-timerem <- minrem*60+secrem
-qtr <- as.numeric(gsub(".*[(][0-9]+:[0-9]+ IN ([1-4]).*", "\\\\1", y))
-
-z <- data.frame(gameid=gameid, team1=team1, team2=team2, score1=score1,
-                score2=score2, timerem=timerem, qtr=qtr,
-                stringsAsFactors=FALSE)
-
-z
-"""
-
+    
 
 def format_game(team1, team2, score1, score2, timerem, qtr):
+    team1 = clean(team1)
+    team2 = clean(team2)
     html = "<div>"
     html += "<p>%s @ %s</p>" % (team1, team2)
     html += "<p> %d - %d " % (int(score1), int(score2))
@@ -63,20 +39,26 @@ def format_game(team1, team2, score1, score2, timerem, qtr):
 
 
 def clean(string):
-    string = str(string).split(" ")[1].strip('\n').strip('"').strip('^')
+    string = string.strip('\n').strip('"').strip('^')
     return string
 
 
 @app.route("/")
 def main():
     html = ""
-    scores = robjects.r(RCode)
-    for i, row in enumerate(scores.iter_row()):
-        row = map(clean, row)
-        game_id, team1, team2, score1, score2, timerem, qtr = row
-        html += format_game(team1, team2, score1, score2,
-                            timerem, qtr)
+    output = subprocess.check_output(["Rscript", "--vanilla", "../get_live_scores.R"])
+    for line in output.split('\n')[1:]:
+        if not line:
+            continue
+        n, gameid, team1, team2, score1, score2, timerem, qtr = line.split(',')
+        html += format_game(team1, team2, score1, score2, timerem, qtr)
     return html
+    # for i, row in enumerate(scores.iter_row()):
+    #     row = map(clean, row)
+    #     game_id, team1, team2, score1, score2, timerem, qtr = row
+    #     html += format_game(team1, team2, score1, score2,
+    #                         timerem, qtr)
+    # return html
 
 
 if __name__ == "__main__":
