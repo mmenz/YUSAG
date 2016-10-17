51683d119f093ce0bed12e273aa4811e443a96a5
Michael Menz <michael.menz.developer@gmail.com>
2016-10-16 22:11:40 -0400

    game ids

--- Basketball/LiveGameModel/web_api/main.py
+++ Basketball/LiveGameModel/web_api/main.py
@@ -6,14 +6,62 @@ import requests
 from urlparse import parse_qs
 import os
 import json
-from flask import Flask, send_from_directory
+from flask import (
+    Flask,
+    send_from_directory,
+    make_response,
+    request,
+    current_app)
 from retrieve_game import make_data_for_game_id
+from functools import update_wrapper
 
 
 app = Flask(__name__)
 
 with open('no_possession_model.mdl') as serialized:
     model = pickle.loads(serialized.read())
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 
 def compute_seconds_remaining(timerem, qtr):
@@ -54,12 +102,14 @@ def send_js(path):
 
 
 @app.route("/<string:game_id>")
+@crossdomain(origin='*')
 def display_game(game_id):
     data = make_data_for_game_id(game_id)
     return json.dumps({"data": data, "gameid": game_id})
 
 
 @app.route("/games")
+@crossdomain(origin='*')
 def list_games_today():
     html = ''
     r = requests.get('http://www.espn.com/nba/bottomline/scores')
