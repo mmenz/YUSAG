5fdbe47f6ba3b55e00090eab581f1e0f7ed9aea6
Michael Menz <michael.menz.developer@gmail.com>
2016-10-25 20:39:10 -0400

    Adding score to description

--- Basketball/LiveGameModel/web_api/retrieve_game.py
+++ Basketball/LiveGameModel/web_api/retrieve_game.py
@@ -25,7 +25,7 @@ def parse_quarter_table(quarter, period, game_output, line):
         diff, total = compute_point_features(score)
         p = model.predict_proba([[diff, seconds, line, total]])
         game_output.append({"time": 2880 - seconds - len(game_output) / 10000.,
-                            "desc": quarter['PLAY'][key],
+                            "desc": score + ', ' + quarter['PLAY'][key],
                             "value": p[0][1],
                             "id": "prediction"})
 
